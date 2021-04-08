d16b0558b2d60f742f92cc4968d63596d40f9be4
Victor Igne Franco Rocha <victor.i.f.rocha@gmail.com>
2021-04-08 20:20:47 -0300

    Criando a primeira apresentação

---
+++ Ola.py
@@ -0,0 +1,3 @@
+Print('''Olá, meu Nome é Victor Igne e sou estudante técnico de Eletrônica pelo CEFET-RJ/MARACANÃ
+Estou aprendendo a programar em Python e em JavaScript/HTML/CSS
+Visando sempre uma auto-evolução para me tornar um bom programador!!!''')
